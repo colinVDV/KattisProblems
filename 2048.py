1f6d44f3ba11cba050e3301cefd261efea1e343b
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2021-10-25 19:41:23 -0500

    Create 2048.py

---
+++ 2048.py
@@ -0,0 +1,12 @@
+#https://open.kattis.com/problems/2048
+#still in progress
+game = [[], [], [], []]
+for i in range(4):
+    temp = input()
+    array = list(map(int, temp.split()))
+    game[i] = array
+action = int(input())
+
+for i in range [4]:
+    for j in range[4]:
+        
