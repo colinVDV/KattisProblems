3d881697ad725ad3cb9920aac532ab7a62f18f01
Colin Vande Vijvere <60828613+colinVDV@users.noreply.github.com>
2021-10-30 23:12:20 -0500

    Update heartRate.py

--- heartRate.py
+++ heartRate.py
@@ -1,3 +1,4 @@
+#https://open.kattis.com/submissions/7930235
 for _ in range(int(input())):
     beats, seconds = [float(x) for x in input().split()]
     print(f'{60*((beats-1)/seconds)} {(60*beats)/seconds} {60*((beats+1)/seconds)}')
